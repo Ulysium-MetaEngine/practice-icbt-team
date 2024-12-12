d8653919fd499deb6fc6dfc6d820f7f03c0392d8
Umedhya Indeewari <umedhyaindeewari@gmail.com>
2024-12-13 00:17:19 +0530

    database script created

---
+++ Umedhya/database-scripts/insert-sample-users.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (email, password) VALUES
+('johndoe@example.com', '$2y$10$eImiTXuWVxfM37uY4JANjQz6s5B9EbWDVhSLNINcxC/U7Ex8bXT5a'), -- "password123"
+('janesmith@example.com', '$2y$10$B6sLVXWgz3vZCnIhS6AXdu2zhLXTb0OpGWS5M8Zxz7Bcn0F3mOme6'), -- "mypassword"
+('alicejohnson@example.com', '$2y$10$GUCsUyz79dP5YHx4d3eFi.7AKNOpf.O/BFB3Nx8M5.D3CXFihyTyW'); -- "securepass"
