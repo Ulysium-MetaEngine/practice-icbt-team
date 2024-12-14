6de80ac9b3a9fcbe56f9b2c2093ec7eeadb6bd66
ImashiRodrigo <imashikawya@gmail.com>
2024-12-14 12:13:19 +0530

    Database connection successful

--- Imashi/Function.dbh.php
+++ Imashi/Function.dbh.php
@@ -3,7 +3,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "registration";
+$dbname = "register_db";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
